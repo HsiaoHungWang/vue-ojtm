086dd3809c04258348b3d246e4b9ca1a6b4b96eb
HsiaoHungWang <wang@ispan.com.tw>
2025-06-24 21:00:05 +0800

    建立及使用組件

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
+import FirstComponent from './components/FirstComponent.vue';
+
+
+
 </script>
 
 <template>
   <h2>Vue Home</h2>
+  <FirstComponent />
 </template>
 
-<style scoped>
-h2 {
-  color: green
-}
-</style>
+<style scoped></style>
---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <h2>第一個 Component</h2>
+</template>
+<style scoped>
+h2 {
+    color: red;
+}
+</style>
+<script setup>
+
+</script>
