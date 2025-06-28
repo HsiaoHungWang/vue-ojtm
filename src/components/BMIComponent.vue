86770aabf903870a6bff6e0f2b9785243afdf354
HsiaoHungWang <wang@ispan.com.tw>
2025-06-28 13:50:37 +0800

    BMI計算介面

---
+++ src/components/BMIComponent.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <div><label>體重(kg)：
+                <input type="number" name="weight" /></label></div>
+        <div>
+            <label>身高(cm)：
+                <input type="number" name="height" /></label>
+        </div>
+        <p>BMI：</p>
+
+    </div>
+</template>
+
+<style lang="css" scoped></style>
