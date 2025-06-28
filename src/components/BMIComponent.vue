c27407ddff70f6acaef87b3a8da90a453ce79d97
HsiaoHungWang <wang@ispan.com.tw>
2025-06-28 14:06:30 +0800

    BMI 計算使用 computed

--- src/components/BMIComponent.vue
+++ src/components/BMIComponent.vue
@@ -1,16 +1,20 @@
 <script setup>
+import { computed, ref } from 'vue';
 
+const weight = ref(70);
+const height = ref(170);
+const bmi = computed(() => (weight.value / ((height.value / 100) ** 2)).toFixed(2))
 </script>
 
 <template>
     <div>
         <div><label>體重(kg)：
-                <input type="number" name="weight" /></label></div>
+                <input type="number" name="weight" v-model.lazy.number="weight" /></label></div>
         <div>
             <label>身高(cm)：
-                <input type="number" name="height" /></label>
+                <input type="number" name="height" v-model.lazy.number="height" /></label>
         </div>
-        <p>BMI：</p>
+        <p>BMI：{{ bmi }}</p>
 
     </div>
 </template>
