d1ba76298aa3ba8a9d74c82fc39bb5b902b6f9c2
HsiaoHungWang <wang@ispan.com.tw>
2025-06-28 13:47:09 +0800

    溫度轉換使用computed

--- src/components/ComputedDemo.vue
+++ src/components/ComputedDemo.vue
@@ -1,13 +1,26 @@
 <script setup>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
 const celsius = ref(0);
+
+const convertToFahrenheit = () => {
+    console.log('convertToFahrenheit called');
+    return (celsius.value * 9 / 5) + 32;
+};
+
+// const fahrenheit = computed(() => {
+//     console.log('fahrenheit computed called');
+//     return (celsius.value * 9 / 5) + 32;
+// });
+const fahrenheit = computed(() => (celsius.value * 9 / 5) + 32);
 </script>
 
 <template>
     <div>
         <label>攝氏：<input type="text" v-model.lazy="celsius"></label>
         <p>華氏：{{ (celsius * 9 / 5) + 32 }}</p>
+        <p>華氏：{{ convertToFahrenheit() }}{{ convertToFahrenheit() }}{{ convertToFahrenheit() }}</p>
+        <p>華氏：{{ fahrenheit }}{{ fahrenheit }}{{ fahrenheit }}</p>
     </div>
 </template>
 
