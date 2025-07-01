2f442a477767e0f6d913bc891547bc39624ea33d
HsiaoHungWang <wang@ispan.com.tw>
2025-07-01 20:07:10 +0800

    路由的相關設定說明

--- src/router.js
+++ src/router.js
@@ -6,28 +6,36 @@ import Team1View from "./views/Team1View.vue";
 import Team2View from "./views/Team2View.vue";
 import MemberCenter from "./views/MemberCenter.vue";
 import TodosView_Full from "./views/TodosView_Full.vue";
+import NotFound from "./views/NotFound.vue";
 
 
 //URL <> Component
 //路由對應表
 const routes = [
-    //http://localhost:5173/  > HomeView
-    {path: "/", component: HomeView, name: "home"},
-    //http://localhost:5173/about  > AboutView
+    {path:"/", redirect: "/home"}, //重定向到 /home
+    //http://localhost:5173/home  > HomeView
+    {path: "/home", component: HomeView, name: "home"},
+    //http://localhost:5173/about_ispan  > AboutView
     {path:"/about_ispan", component: AboutView, name: "about",
+        redirect:"/about_ispan/team1", //重定向到 team1
         children:[
-            //http://localhost:5173/about/team1 > Team1View 
+            //http://localhost:5173/about_ispan/team1 > Team1View 
             {path: "team1", component: Team1View, name: "team1"},
-            //http://localhost:5173/about/team2 > Team2View 
+            //http://localhost:5173/about_ispan/team2 > Team2View 
             {path: "team2", component: Team2View, name: "team2"},
         ]
     },
-    //http://localhost:5173/xyz  > ContactView
-    {path:"/contact", component: ContactView, name: "contact"},
+    //http://localhost:5173/contact  > ContactView
+    {path:"/contact/", component: ContactView, name: "contact",
+     alias: "/contact_us", //別名
+     sensitive: false, //大小寫敏感
+     strict:false //嚴格模式
+    },
     //http://localhost:5173/member  > MemberCenter
     {path:"/member/:id", component:MemberCenter, name:"member",props:true},
-        //http://localhost:5173/todos  > MemberCenter
-    {path:"/todos", component:TodosView_Full, name:"todos"}
+        //http://localhost:5173/todos  > TodosView_Full
+    {path:"/todos", component:TodosView_Full, name:"todos"},
+    {path:'/:pathMatch(.*)*',component:NotFound, name:"notfound"}
 ]
 
 const router = createRouter({
