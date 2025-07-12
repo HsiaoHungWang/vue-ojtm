63352de6e1b59a3476fda74bc685fd24874a8d3f
HsiaoHungWang <wang@ispan.com.tw>
2025-07-12 09:51:16 +0800

    會員新增UI

--- src/views/MemeberView.vue
+++ src/views/MemeberView.vue
@@ -1,4 +1,5 @@
 <script setup>
+import MemberAdd from '@/components/MemberAdd.vue';
 import { ref } from 'vue';
 
 
@@ -9,7 +10,7 @@ const staticUrl = `${import.meta.env.VITE_STATICURL}/images`
 
 //讀取所有會員資料
 const loadMembers = async () => {
-    const response = await fetch(apiUrl);
+    const response = await fetch(apiUrl);  //GET
     const datas = await response.json();
     members.value = datas;
 }
@@ -36,28 +37,38 @@ loadMembers();
 <template>
     <div>
         <h3>會員管理</h3>
-        <table class="table table-bordered table-hover table-striped">
-            <thead>
-                <tr>
-                    <th>頭像</th>
-                    <th>姓名</th>
-                    <th>電子郵件</th>
-                    <th>年紀</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="member in members" :key="member.memberId">
-                    <td><img :src="`${staticUrl}/${member.fileName}`" class="img-thumbnail" style="width:100px" /></td>
-                    <td>{{ member.name }}</td>
-                    <td>{{ member.email }}</td>
-                    <td>{{ member.age }}</td>
-                    <td>
-                        <button @click="deleteHandler(member.memberId)" class="btn btn-danger">刪除</button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        <div class="row">
+            <div class="col-9">
+                <table class="table table-bordered table-hover table-striped">
+                    <thead>
+                        <tr>
+                            <th>頭像</th>
+                            <th>姓名</th>
+                            <th>電子郵件</th>
+                            <th>年紀</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="member in members" :key="member.memberId">
+                            <td><img :src="`${staticUrl}/${member.fileName}`" class="img-thumbnail"
+                                    style="width:100px" /></td>
+                            <td>{{ member.name }}</td>
+                            <td>{{ member.email }}</td>
+                            <td>{{ member.age }}</td>
+                            <td>
+                                <button @click="deleteHandler(member.memberId)" class="btn btn-danger"><i
+                                        class="bi bi-trash-fill"></i> 刪除</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-3">
+                <MemberAdd></MemberAdd>
+            </div>
+        </div>
+
     </div>
 </template>
 
